4890ea9edacf334c8fa2911a30392c9be8a83897
Roman Timoshkin <rmntim@yandex.ru>
2024-03-04 13:44:54 +0300

    feat: add logging initialization

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/rmntim/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	config := config.MustLoad()
 
-	// TODO: init logging
+	logger := newLogger(config.Env)
+	logger.Info("starting application",
+		slog.String("env", config.Env),
+		slog.Any("config", config),
+		slog.Int("port", config.Grpc.Port),
+	)
 
 	// TODO: init app
 
 	// TODO: start gRPC server
+}
+
+func newLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
 }
