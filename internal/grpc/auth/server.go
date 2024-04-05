8cd67f9af1b14aa46e48f90b4eed8fe0d3cbd8fc
Roman Timoshkin <rmntim@yandex.ru>
2024-04-05 10:55:49 +0300

    feat: implement grpc functionality
    validation, passing the data to services, and sending responses...

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,24 +5,113 @@ import (
 
 	"github.com/rmntim/sso/contracts/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appId int,
+	) (string, error)
+	Register(
+		ctx context.Context,
+		email string,
+		password string,
+	) (int64, error)
+	IsAdmin(ctx context.Context, userId int64) (bool, error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
-	panic("unimplemented")
+	if err := validateLoginRequest(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Login(ctx, req.Email, req.Password, int(req.AppId))
+	if err != nil {
+		// TODO: handle error type
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: token,
+	}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
-	panic("unimplemented")
+	if err := validateRegisterRequest(req); err != nil {
+		return nil, err
+	}
+
+	userId, err := s.auth.Register(ctx, req.Email, req.Password)
+	if err != nil {
+		// TODO: handle error type
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.RegisterResponse{
+		UserId: userId,
+	}, nil
 }
 
 func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
-	panic("unimplemented")
+	if err := validateIsAdminRequest(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.UserId)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
+func validateLoginRequest(req *ssov1.LoginRequest) error {
+	if req.Email == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.Password == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.AppId == 0 {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+func validateRegisterRequest(req *ssov1.RegisterRequest) error {
+	if req.Email == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.Password == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+func validateIsAdminRequest(req *ssov1.IsAdminRequest) error {
+	if req.UserId == 0 {
+		return status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	return nil
 }
