3c132e26dfef114eaad2ecf0a525c6ef15455d78
Roman Timoshkin <rmntim@yandex.ru>
2024-04-14 09:22:44 +0300

    test: add misc files for testing

---
+++ tests/suite/suite.go
@@ -0,0 +1,54 @@
+package suite
+
+import (
+	"context"
+	"net"
+	"strconv"
+	"testing"
+
+	authv1 "github.com/rmntim/sso/contracts/gen/go/auth/v1"
+	"github.com/rmntim/sso/internal/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Suite struct {
+	*testing.T
+	Cfg        *config.Config
+	AuthClient authv1.AuthServiceClient
+}
+
+const grpcHost = "localhost"
+
+func New(t *testing.T) (context.Context, *Suite) {
+	t.Helper()
+	t.Parallel()
+
+	cfg := config.MustLoadFromPath("../config/local_tests.yaml")
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Grpc.Timeout)
+
+	t.Cleanup(func() {
+		t.Helper()
+		cancel()
+	})
+
+	cc, err := grpc.DialContext(
+		context.Background(),
+		grpcAddress(cfg),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		t.Fatalf("grpc server connection failed: %v", err)
+	}
+
+	return ctx, &Suite{
+		T:          t,
+		Cfg:        cfg,
+		AuthClient: authv1.NewAuthServiceClient(cc),
+	}
+}
+
+func grpcAddress(cfg *config.Config) string {
+	return net.JoinHostPort(grpcHost, strconv.Itoa(cfg.Grpc.Port))
+}
